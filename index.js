5cf4d01a481d52d38fd89e11c7f93493a377c86b
mrsaiful778 <mrsaifulislam778@gmail.com>
2023-10-21 19:00:54 +0600

    update completed

--- index.js
+++ index.js
@@ -66,6 +66,27 @@ async function run() {
     
    })
 
+   app.put('/products/:id', async(req, res) => {
+    const id = req.params.id;
+    const itemes = req.body
+    const filter = { _id: new ObjectId(id)};
+    const options = { upsert: true };
+    const updateItem = { 
+      $set: {
+        name: itemes.name,
+        photo: itemes.photo,
+        price: itemes.price,
+        rating: itemes.rating,
+        brand: itemes.brand,
+        category: itemes.category,
+      }
+    }
+    const  result = await userCollection.updateOne(filter, updateItem, options)
+    res.send(result)
+
+
+   })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
