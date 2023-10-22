642ffde23cb25db9e99402bbeb2672c8d4c725a7
mrsaiful778 <mrsaifulislam778@gmail.com>
2023-10-22 16:31:41 +0600

    delet carts

--- index.js
+++ index.js
@@ -36,17 +36,7 @@ async function run() {
     const userCollection = client.db('EcommerceDB').collection('user');
     const detailsCollection = client.db('detailsDB').collection('details');
 
-    app.post('/carts', async (req, res) => {
-      const cart = req.body;
-      console.log(cart);
-      const result = await detailsCollection.insertOne(cart);
-      res.send(result);
-  })
-  
-  app.get('/carts', async (req, res) => {
-      const result = await detailsCollection.find().toArray();
-      res.send(result);
-  })
+   
 
     app.post('/products', async (req, res) => {
       const user = req.body;
@@ -84,6 +74,27 @@ async function run() {
     const  result = await userCollection.updateOne(filter, updateItem, options)
     res.send(result)
 
+
+   })
+
+    app.post('/carts', async (req, res) => {
+      const cart = req.body;
+      console.log(cart);
+      const result = await detailsCollection.insertOne(cart);
+      res.send(result);
+  })
+  
+  app.get('/carts', async (req, res) => {
+      const result = await detailsCollection.find().toArray();
+      res.send(result);
+  })
+
+   app.delete('/carts/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) };
+        const result = await detailsCollection.deleteOne(query)
+        res.send(result)
+        console.log(result);
 
    })
 
